e8fcf27c2baa0460d0c88e0e7a047b63fb9b49bb
Flávio Pontes <flaviopontes@roquettepinto.org.br>
2016-11-21 11:07:13 -0200

    First Test not compiling

---
+++ src/BowlingGame/BowlingTest.java
@@ -0,0 +1,16 @@
+package BowlingGame;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class BowlingTest {
+
+    @Test
+    public void nothing() throws Exception {
+
+    }
+
+}
