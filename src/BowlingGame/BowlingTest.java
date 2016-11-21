3a5809aa08425e796e9ed3f10b82078bdad0e259
Flávio Pontes <flaviopontes@roquettepinto.org.br>
2016-11-21 11:24:35 -0200

    Removed duplicate code in the test

--- src/BowlingGame/BowlingTest.java
+++ src/BowlingGame/BowlingTest.java
@@ -1,16 +1,41 @@
 package BowlingGame;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 
 public class BowlingTest {
+    private Game g;
+
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i< n; i++) {
+            g.roll(pins);
+        }
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        g = new Game();
+    }
 
     @Test
-    public void nothing() throws Exception {
+    public void canRoll() throws Exception {
+        g.roll(0);
+    }
 
+    @Test
+    public void gutterGame() throws Exception {
+        rollMany(20, 0);
+        assertEquals(0, g.score());
+    }
+
+    @Test
+    public void allOnes() throws Exception {
+        rollMany(20, 1);
+        assertEquals(20, g.score());
     }
 
 }
